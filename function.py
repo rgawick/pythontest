571fe60a8fe756f0ab01cae8c58f459cd186d6a5
Ryan Gawick <rgawick@gmail.com>
2018-09-18 11:45:34 -0500

    testing

---
+++ function.py
@@ -0,0 +1,11 @@
+def display_car_information(make, model):
+    print("--------------")
+    print("Make {0} - Model {1}".format(make, model))
+
+display_car_information("Ford", "Mustang")
+
+def even_or_odd(number):
+    if(number % 2 == 0):
+        print("Even")
+    else:
+        print("Not equal")
